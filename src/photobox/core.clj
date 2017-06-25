645cf9b45a65b6ca0b4e70c3423cd6ee2a074c73
Adam Lloyd <adam@alloy-d.net>
2017-06-25 08:26:04 -0400

    Make Exif processing code less disgusting.

--- src/photobox/core.clj
+++ src/photobox/core.clj
@@ -1,10 +1,10 @@
 (ns photobox.core
   (:require [clojure.pprint :refer (pprint)]
             [clojure.set :as set]
-            [exif-processor.core :refer (exif-for-filename)]))
+            [exif-processor.core :as processor]))
 
 (def test-file "/Volumes/Untitled/DCIM/103_FUJI/DSCF3788.JPG")
-(def interesting-data #{"Date/Time"
+(def interesting-tags #{"Date/Time"
                         "Development Dynamic Range"
                         "Dynamic Range"
                         "Dynamic Range Setting"
@@ -53,43 +53,44 @@
    "0x1438" {:name "Image Count"
              :value #(Integer/parseInt %)}})
 
-(defn available-data [filename]
-  (keys (exif-for-filename filename)))
-
-(defn visit-file [filename]
-  (exif-for-filename filename))
-
-(defn unknown-tag-number [tag-name]
+(defn- unknown-tag-number [tag-name]
+  "Parses the numerical component of auto-generated unknown tag names."
   (if-let [matches (re-matches #"Unknown tag \((0x[0-9a-f]+)\)" tag-name)]
     (nth matches 1)))
 
-(defn postprocess [data]
-  (letfn [(postprocess-single [[k v]]
-              (if-let [number (unknown-tag-number k)]
-                (if-let [postprocessor (postprocessors number)]
-                  {(:name postprocessor) ((:value postprocessor) v)}
-                  {k v})
-                {k v}))]
+(defn- postprocess [data]
+  "Supplements exif-processor's data with extra stuff we know how to interpret."
+  (letfn [(postprocess-single [[tag value]]
+            (let [number (unknown-tag-number tag)
+                  postprocessor (if number (postprocessors number))]
+              (if postprocessor
+                {(:name postprocessor) ((:value postprocessor) value)}
+                {tag value})))]
     (apply merge (map postprocess-single data))))
 
-(defn extract-interesting [data]
-  (filter (fn [[k _]] (interesting-data k)) data))
+(def exif-for-filename
+  "Same as exif-processor.core/exif-for-filename, but with our very specific postprocessing."
+  (comp postprocess processor/exif-for-filename))
 
-(defn missing-data [data]
-  (set/difference interesting-data (set (keys data))))
+(def available-tags-for-filename
+  "Lists Exif data keys available in a file."
+  (comp keys exif-for-filename))
+
+(defn interesting-data [data]
+  "Returns the interesting stuff from extracted Exif data."
+  (select-keys data interesting-tags))
+
+(defn missing-tags [data]
+  "Lists Exif data keys that we're interested in but don't have in the given data."
+  (set/difference interesting-tags (set (keys data))))
 
 (defn unknown-data [data]
-  (apply merge (map (fn [[k v]] {(unknown-tag-number k) v})
-                    (filter (fn [[k _]] (unknown-tag-number k)) data))))
-(defn unknown-data [data]
-  (filter (fn [[k _]] (unknown-tag-number k)) data))
+  "Returns the unknown stuff from extracted Exif data."
+  (filter (fn [[tag _]] (unknown-tag-number tag)) data))
 
-(def print-interesting (comp pprint (partial sort-by first) extract-interesting postprocess))
-(def print-unknown (comp pprint (partial sort-by first) unknown-data))
-(def print-postprocessed (comp pprint (partial sort-by first) postprocess))
-(def print-postprocessed-unknowns (comp pprint (partial sort-by first) postprocess unknown-data))
-
-(extract-interesting (exif-for-filename test-file))
-(missing-data (extract-interesting (exif-for-filename test-file)))
-(unknown-data (exif-for-filename test-file))
-
+(def interesting-data-for-filename
+  (comp interesting-data exif-for-filename))
+(def missing-tags-for-filename
+  (comp missing-tags exif-for-filename))
+(def unknown-data-for-filename
+  (comp unknown-data exif-for-filename))
