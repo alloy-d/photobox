9c44f9883cab8f20c326b1c97747b803dfa9a6ca
Adam Lloyd <adam@alloy-d.net>
2017-07-02 18:46:04 -0400

    Add a test for the easiest part. :-D

---
+++ test/photobox/fs_test.clj
@@ -0,0 +1,22 @@
+(ns photobox.fs-test
+  (:require [photobox.fs :refer :all]
+            [me.raynes.fs :as fs]
+            [clojure.java.io :refer [file]]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]))
+
+(def extension (gen/elements [".RAF", ".JPG"]))
+(def filename-gen
+  (gen/fmap (fn [[name extension]]
+              (str name extension))
+            (gen/tuple (gen/not-empty gen/string-alphanumeric) extension)))
+(def file-gen (gen/fmap file filename-gen))
+
+(defspec all-jpegs-come-first 100
+  (prop/for-all [v (gen/not-empty (gen/vector file-gen))]
+                (let [sorted-by-extension (sort-by-extension v)
+                      sorted-jpegs-only (sort (filter #(= (fs/extension %) ".JPG") v))]
+                  (= sorted-jpegs-only
+                     (take (count sorted-jpegs-only) sorted-by-extension)))))
+;(defspec all-jpegs-come-first 100 )
