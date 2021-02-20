ab1cfdfc3eb56c97f2c2ffe32bc121a21e323a2e
Adam Lloyd <adam@alloy-d.net>
2021-02-20 14:14:29 -0500

    Tweak field names printed by OSA lister.

--- scripts/osa/list-favorite-photos.js
+++ scripts/osa/list-favorite-photos.js
@@ -1,9 +1,12 @@
 const getFavorites = () => Application("Photos").favoritesAlbum().mediaItems();
+const getAll = () => Application("Photos").mediaItems();
 
 const getArchivePath = (date, filename) => {
   const [year, month, day] = date.toISOString().split('T')[0].split('-');
 
-  return `${year}/${year}-${month}/${year}-${month}-${day}/${year}${month}${day}-${filename}`
+  const archiveFilename = /^\d{8}-/.test(filename) ? filename.split('-')[1] : filename;
+
+  return `${year}/${year}-${month}/${year}-${month}-${day}/${year}${month}${day}-${archiveFilename}`
 };
 
 const getBasics = (item) => {
@@ -14,14 +17,20 @@ const getBasics = (item) => {
   const archivePath = getArchivePath(date, filename);
 
   return {
-    id,
+    'apple-photos-id': id,
     filename,
     date,
-    archivePath,
+    'archive-path': archivePath,
   };
 };
 
 const isCameraPhoto = ({filename}) => filename.match(/(DSCF\d{4}|R\d{7}).JPG/)
 
 const report = (stuff) => console.log(JSON.stringify(stuff, null, 2));
-report(getFavorites().map(getBasics).filter(isCameraPhoto));
+
+function run(argv) {
+  const photos = argv[0] === '--all' ? getAll() : getFavorites();
+
+  const output = photos.map(getBasics).filter(isCameraPhoto);
+  return JSON.stringify(output, null, 2);
+}
