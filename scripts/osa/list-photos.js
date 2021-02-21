f80ead97eb0bed19098a3539bb11ff299787e133
Adam Lloyd <adam@alloy-d.net>
2021-02-21 11:22:36 -0500

    Tweak photos lister output and interface again.

--- scripts/osa/list-photos.js
+++ scripts/osa/list-photos.js
@@ -13,23 +13,25 @@ const getBasics = (item) => {
   const id = item.id();
   const filename = item.filename();
   const date = item.date();
+  const favorite = item.favorite();
 
   const archivePath = getArchivePath(date, filename);
 
   return {
-    'apple-photos-id': id,
-    'original-filename': filename,
+    id,
     date,
+    favorite,
+    'original-filename': filename,
     'archive-path': archivePath,
   };
 };
 
-const isCameraPhoto = ({filename}) => filename.match(/(DSCF\d{4}|R\d{7}).JPG/)
+const isCameraPhoto = (photo) => photo['original-filename'].match(/(DSCF\d{4}|R\d{7}).JPG/)
 
 const report = (stuff) => console.log(JSON.stringify(stuff, null, 2));
 
 function run(argv) {
-  const photos = argv[0] === '--all' ? getAll() : getFavorites();
+  const photos = argv[0] === '--favorites' ? getFavorites() : getAll();
 
   const output = photos.map(getBasics).filter(isCameraPhoto);
   return JSON.stringify(output, null, 2);
