202ddcb592d9f06de15b279740ea48ef1c14a0d1
Adam Lloyd <adam@alloy-d.net>
2021-04-03 10:32:21 -0400

    Don't bother generating an untrustworthy archive-path from Apple Photos.

--- scripts/osa/list-photos.js
+++ scripts/osa/list-photos.js
@@ -1,28 +1,17 @@
 const getFavorites = () => Application("Photos").favoritesAlbum().mediaItems();
 const getAll = () => Application("Photos").mediaItems();
 
-const getArchivePath = (date, filename) => {
-  const [year, month, day] = date.toISOString().split('T')[0].split('-');
-
-  const archiveFilename = /^\d{8}-/.test(filename) ? filename.split('-')[1] : filename;
-
-  return `${year}/${year}-${month}/${year}-${month}-${day}/${year}${month}${day}-${archiveFilename}`
-};
-
 const getBasics = (item) => {
   const id = item.id();
   const filename = item.filename();
   const date = item.date();
   const favorite = item.favorite();
 
-  const archivePath = getArchivePath(date, filename);
-
   return {
     id,
     date,
     favorite,
     'original-filename': filename,
-    'archive-path': archivePath,
   };
 };
 
